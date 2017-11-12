e92761eabd69fce6dc3b222916cfe710f36322c1
Denis Cheremisov <d.cheremisov@corp.mail.ru>
2017-11-12 14:12:39 +0300

    get rid of tiny file

--- tokenizer.go
+++ tokenizer.go
@@ -44,6 +44,34 @@ type Code struct {
 // Comment represents everything else
 type Comment String
 
+// Integer integer number
+type Integer struct {
+	Location
+	Value string
+	Real  int64
+}
+
+// Unsigned unsigned integer number
+type Unsigned struct {
+	Location
+	Value string
+	Real  uint64
+}
+
+// Float represents floating point number
+type Float struct {
+	Location
+	Value string
+	Real  float64
+}
+
+// Boolean represents
+type Boolean struct {
+	Location
+	Value string
+	Real  bool
+}
+
 // tokenizer serializes input stream into the set of tokens
 type tokenizer struct {
 	input   []byte
